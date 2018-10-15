648454c89e9a4491cbd75f717241ec26f9278553
Megan Kaluza <megankaluza@Megans-MacBook-Pro.local>
2018-10-15 10:46:36 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# _Personal Portfolio_
+
+#### By _**Megan Kaluza**_
+
+# _Live_
+![screenshot](img/screen_shot.png)
+
+## Description
+
+_Objectives_
+
+* _Upload and showcase polished websites and mockups._
+
+## Setup/Installation Requirements
+
+* _mkaluza.com_
+* _Check out the GitHub Pages: https://megankaluza.github.io/personal-portfolio/_
+* _Clone this repository https://github.com/megankaluza/personal-portfolio.git_
+* _gem install sass_
+* _gem install bourbon_
+* _gem install neat_
+* _gem install saffron_
+* _sass --watch ._
+* _Open in browser of your choice_
+
+## Known Bugs
+
+_No known bugs at this time_
+
+## Support and contact details
+
+  _megan.kaluza@gmail.com_
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _SASS_
+* _Flexbox_
+
+### License
+
+Copyright (c) 2016 **_Megan Kaluza_**
+
+This software is licensed under the MIT license.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ js/scripts.js
@@ -0,0 +1,172 @@
+//Time and Date Stamp
+var MONTH_NAME = ['January', 'Febuary', 'March', 'April', 'May', 'June',
+                  'July', 'August', 'September', 'October', 'November', 'December'];
+function showTime() {
+    function twoDigit(n) {
+        return ('0' + n).slice(-2);
+    }
+    function iso8601(date) {
+        return date.getFullYear() +
+               '-' + twoDigit(1 + date.getMonth()) +
+               '-' + twoDigit(date.getDate()) +
+               'T' + twoDigit(date.getHours()) +
+               ':' + twoDigit(date.getMinutes());
+    }
+    function en_US(date) {
+        var h = date.getHours() % 12;
+        return MONTH_NAME[date.getMonth()] +
+               ' '  + date.getDate() +
+               ', ' + date.getFullYear() +
+               '<br />' + (h == 0 ? 12 : h) +
+               ':'  + twoDigit(date.getMinutes()) +
+               ' ' + (date.getHours() < 12 ? 'am' : 'pm');
+    }
+
+    var timeEl = document.getElementById('time');
+    if (timeEl !== null) {
+        var now = new Date();
+        timeEl.innerHTML = en_US(now);
+        timeDiv.setAttribute('datetime', iso8601(now));
+    }
+};
+setInterval(showTime, 1000);
+
+$(document).ready(function(){
+  //Smooth Scrolling
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  //Open Modal
+  $('#open').click(function(){
+    $('.ten')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  //Exit Modal
+  $('.exit').click(function(){
+    $('.modal').modal('hide');
+  });
+  $('#open2').click(function(){
+    $('.eleven')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#open3').click(function(){
+    $('.twelve')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#open4').click(function(){
+    $('.thirteen')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#open5').click(function(){
+    $('.fourteen')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#open6').click(function(){
+    $('.fifteen')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#open7').click(function(){
+    $('.insta')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#open8').click(function(){
+    $('.sixteen')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#open9').click(function(){
+    $('.seventeen')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#open10').click(function(){
+    $('.eighteen')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#open11').click(function(){
+    $('.nineteen')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#open12').click(function(){
+    $('.twenty')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#openIcons').click(function(){
+    $('.icons')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#openGreen').click(function(){
+    $('.green')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#openRed').click(function(){
+    $('.red')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#openBlue').click(function(){
+    $('.blue')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+  $('#openEyes').click(function(){
+    $('.eyes')
+    .modal({
+      blurring: true
+    })
+    .modal('show');
+  });
+});
